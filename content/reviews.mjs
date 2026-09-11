684777341134fd563e15754a9c013c21dbfcfc6d
Ivo Stromidlo <ivostromidlo@Ivos-iMac.local>
2026-09-11 12:15:05 +0300

    Use authentic Google reviewer avatars and remove review arrows

--- content/reviews.mjs
+++ content/reviews.mjs
@@ -1,9 +1,9 @@
 // Manual Google Maps snapshot checked on 2026-09-11.
 export const reviewSource = 'https://maps.google.com/maps?ftid=0x46eee77eef601e47:0x5ac3c96f8826ce3d';
 export const customerReviews = [
-  {author:'Edīte Grunde', initials:'EG', url:'https://maps.app.goo.gl/HvN2o2Ghrw5kHYeMA', quote:'Brīnišķīgas telpas. Gaišas, plašas, mājīgas ar burvīgu virtuvīti un jauku spēļu zonu mazajiem. Vienmēr tīras un sakoptas.'},
-  {author:'Es - Sieviete', initials:'ES', url:'https://maps.app.goo.gl/NWoy9wPpMy3tPGUY7', quote:'Gaiša, patīkama atmosfēra! Kopā ar bērniņu apmeklējām brīvās rotaļāšanās nodarbību Room Jūrmala. Telpas plašas, piemērotas arī citām aktivitātēm.'},
-  {author:'Agnese Čimale', initials:'AČ', url:'https://maps.app.goo.gl/tvwkV6u4uX3NXYLe7', quote:'Paldies par iespēju un ļoti vērtīgi, ka kaut kas tāds ir pieejams Jūrmalas mammām!'},
+  {author:'Edīte Grunde', initials:'EG', avatar:'https://lh3.googleusercontent.com/a/ACg8ocKCgtTH9hnbuQKDsbCOVMPyd39qJhZskTWsW0NXpU2Wo_RgSg=w72-h72-p-rp-mo-br100', url:'https://maps.app.goo.gl/HvN2o2Ghrw5kHYeMA', quote:'Brīnišķīgas telpas. Gaišas, plašas, mājīgas ar burvīgu virtuvīti un jauku spēļu zonu mazajiem. Vienmēr tīras un sakoptas.'},
+  {author:'Es - Sieviete', initials:'ES', avatar:'https://lh3.googleusercontent.com/a-/ALV-UjVFfP1K_7c48XwXv89E5VSF7JJ9yRMSwzqYA4MRpVh-xrUE86fv=w72-h72-p-rp-mo-br100', url:'https://maps.app.goo.gl/NWoy9wPpMy3tPGUY7', quote:'Gaiša, patīkama atmosfēra! Kopā ar bērniņu apmeklējām brīvās rotaļāšanās nodarbību Room Jūrmala. Telpas plašas, piemērotas arī citām aktivitātēm.'},
+  {author:'Agnese Čimale', initials:'AČ', avatar:'https://lh3.googleusercontent.com/a-/ALV-UjW2VRyfeBA2hTTEZAe5lRpqYnrigBeYG_JbzjOMzxZDgKxC3Crq=w72-h72-p-rp-mo-br100', url:'https://maps.app.goo.gl/tvwkV6u4uX3NXYLe7', quote:'Paldies par iespēju un ļoti vērtīgi, ka kaut kas tāds ir pieejams Jūrmalas mammām!'},
 ];
 export const reviewCopy = {
   lv:{eyebrow:'KLIENTU ATSAUKSMES',title:'Pie mums <em>ir labi.</em>',count:'12 Google atsauksmes',all:'Visas atsauksmes Google',note:'Klientu atsauksmju fragmenti no Google.',original:'Lasīt Google',rating:'5 no 5 zvaigznēm'},
